b67a00e56f371635a4d1bba4873cedb058bd0bcf
Robert Quintana <robertquintanatx@gmail.com>
2020-05-29 09:08:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -16,4 +16,12 @@ end
 
 def sort_array_char_count(int_array)
   int_array.sort do |a,b|
+    if a.length == b.length
+      0 
+    elsif a.length > b.length 
+      1
+    elsif a.length < b.length
+      -1 
+    end
+  end
 end  
