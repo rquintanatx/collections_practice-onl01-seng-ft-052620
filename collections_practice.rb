317de893416504bebf56e9b629c0901024ea42ad
Robert Quintana <robertquintanatx@gmail.com>
2020-05-29 09:02:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,11 +3,11 @@ def sort_array_asc(int_array)
 end
 
 def sort_array_desc(int_array)
-  int_array.sort
-  if a = b 
-    0 
-  elsif a > b 
-    -1 
+  int_array.sort do |a,b|
+    if a = b 
+      0 
+   elsif a > b 
+     -1 
   elsif a < b 
     1
   end
